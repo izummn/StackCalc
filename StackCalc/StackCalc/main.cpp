6bdb27ac032d43edf3502130c1e11688a615dd5a
izummn <maratik1993@mail.ru>
2016-07-31 16:48:16 +0300

    Add double type for Stack struct

--- StackCalc/StackCalc/main.cpp
+++ StackCalc/StackCalc/main.cpp
@@ -14,7 +14,7 @@ const char *POP = "pop";
 
 
 struct Stack{
-	int data[SIZE];
+	double data[SIZE];
 	int count;
 
 	Stack()
@@ -27,57 +27,57 @@ struct Stack{
 };
 
 
-void StackPush(Stack* st, int value)
+void StackPush(Stack* st, double value)
 {
 	st->data[st->count++] = value;	
 }
 
-int StackPop(Stack* st)
+double StackPop(Stack* st)
 {	
 	if (st->count < 1) printf(" Error! ");
 	else
 	{
 		st->count--;
-		int result = st->data[st->count];
+		double result = st->data[st->count];
 		st->data[st->count] = NULL;
 		return result;
 	}
 	return 0;
 }
 
-void Add(Stack* st, int value)
+void Add(Stack* st, double value)
 {
 	StackPush(st, value);
 	st->data[st->count-2] += st->data[st->count - 1];
 	st->count--;
 }
 
-void Mul(Stack* st, int value)
+void Mul(Stack* st, double value)
 {
 	StackPush(st, value);
 	st->data[st->count - 2] *= st->data[st->count - 1];
 	st->count--;
 }
 
-void Div(Stack* st, int value)
+void Div(Stack* st, double value)
 {
 	StackPush(st, value);
 	st->data[st->count - 2] = st->data[st->count - 2] / st->data[st->count - 1];
 	st->count--;
 }
 
-void Sub(Stack* st, int value)
+void Sub(Stack* st, double value)
 {
 	StackPush(st, value);
 	st->data[st->count - 2] = st->data[st->count - 2] - st->data[st->count - 1];
 	st->count--;
 }
 
-void Input(char* operation, int* value)
+void Input(char* operation, double* value)
 {
 	printf(" Welcome to Stack Calculater! \n");
 	printf(" Please, enter operation and value: \n");
-	scanf_s("%s%d", operation, OPERATION_SIZE, value);
+	scanf_s("%s%lg", operation, OPERATION_SIZE, value);
 	
 }
 
@@ -85,25 +85,26 @@ void Output(Stack* st)
 {
 	printf(" Result: \n");
 	for (int i = 0; i < st->count; i++)
-		printf("[%d] = %d;\n", i + 1, st->data[i]);
+		printf("[%d] = %lg;\n", i + 1, st->data[i]);
 	printf("\n");
 }
 
-void StackCalculation(Stack* st, char* operation, int value)
+void StackCalculation(Stack* st, char* operation, double value)
 {
-	if (_stricmp(operation, ADD) == 0) Add(st, value);
-	if (_stricmp(operation, SUB) == 0) Sub(st, value);
-	if (_stricmp(operation, MUL) == 0) Mul(st, value);
-	if (_stricmp(operation, DIV) == 0) Div(st, value);
-	if (_stricmp(operation, PUSH) == 0) StackPush(st, value);
-	if (_stricmp(operation, POP) == 0) StackPop(st);
+	if (_stricmp(operation, ADD) == 0) { Add(st, value); return; };
+	if (_stricmp(operation, SUB) == 0) { Sub(st, value); return; };
+	if (_stricmp(operation, MUL) == 0) { Mul(st, value); return; };
+	if (_stricmp(operation, DIV) == 0) { Div(st, value); return; };
+	if (_stricmp(operation, PUSH) == 0) { StackPush(st, value); return; };
+	if (_stricmp(operation, POP) == 0) { StackPop(st); return; };
+	printf("Incorrect operation! ");
 }
 
 void Start()
 {
 	Stack StackToCalc;
 	char operation[OPERATION_SIZE];
-	int value = 0;
+	double value = 0.0;
 	int t = 0;
 	while (t == 0)
 	{ 
@@ -116,23 +117,23 @@ void Start()
 void TestStackCalculation()
 {
 	Stack StackToCalc;
-	char operation[OPERATION_SIZE];
+	char operation[OPERATION_SIZE] = "";
 	int value = 0;
-	StackCalculation(&StackToCalc, "push", 3);
+	StackCalculation(&StackToCalc, "push", 3.8);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "push", 8);
+	StackCalculation(&StackToCalc, "push", 8.0);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "push", 12);
+	StackCalculation(&StackToCalc, "push", 1.2);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "add", 9);
+	StackCalculation(&StackToCalc, "add", 9.0);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "sub", 8);
+	StackCalculation(&StackToCalc, "sub", 8.6);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "mul", 3);
+	StackCalculation(&StackToCalc, "mul", 3.0);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "push", 87);
+	StackCalculation(&StackToCalc, "push", 87.6);
 	Output(&StackToCalc);
-	StackCalculation(&StackToCalc, "add", 34);
+	StackCalculation(&StackToCalc, "div", 34.4);
 	Output(&StackToCalc);
 	StackCalculation(&StackToCalc, "pop", 0);
 	Output(&StackToCalc);
@@ -142,6 +143,7 @@ int main()
 {
 	TestStackCalculation();
 	//Start();
+
 	return 0;
 }
 
